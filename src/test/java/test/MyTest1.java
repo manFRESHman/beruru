92144f534060a7cd7650aec07f4bf749bdb84098
IgorTolobaev <36775175+manFRESHman@users.noreply.github.com>
2019-03-11 22:36:32 +0400

    new

--- src/test/java/test/MyTest1.java
+++ src/test/java/test/MyTest1.java
@@ -3,15 +3,23 @@ package test;
 import org.junit.Test;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
 
 
 public class MyTest1 {
     @Test
     public void func(){
-
-        open("http://google.com");
-        $(By.name("q")).setValue("Selenide").pressEnter();
+        open("http://beru.ru");
+        sleep(2000);
+        $(By.xpath("/html")).pressEscape();
+        $(By.xpath("//*[text()='Войти в аккаунт']")).click();
+        $(By.xpath("//*[@name='login']")).setValue("beru.ru.test");
+        $(By.className("passp-sign-in-button")).click();
+        $(By.xpath("//*[@name='passwd']")).setValue("beru.ru");
+        $(By.className("passp-sign-in-button")).click();
+        $(By.xpath("//*[@title='Мой профиль']")).exists();
+        $(By.xpath("//*[@title='Мой профиль']")).click();
+        sleep(1000000000);
     }
 }
